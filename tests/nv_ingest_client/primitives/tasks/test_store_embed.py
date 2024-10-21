af21a90765b976fb67db377b91f3d40a3497b77e
Julio Perez <jperez@nvidia.com>
2024-10-21 17:32:56 -0400

    update tests for store and add tests for store embed

--- tests/nv_ingest_client/primitives/tasks/test_store_embed.py
+++ tests/nv_ingest_client/primitives/tasks/test_store_embed.py
@@ -3,25 +3,22 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
-from nv_ingest_client.primitives.tasks.store import StoreTask
+from nv_ingest_client.primitives.tasks.store import StoreEmbedTask
 
 # Initialization and Property Setting
 
 
 def test_store_task_initialization():
-    task = StoreTask(
-        structured=True,
-        images=True,
+    task = StoreEmbedTask(
+        embedding=True,
+        store_method="s3",
         extra_params={
+            "endpoint": "minio:9000",
             "access_key": "foo",
             "secret_key": "bar",
-            "endpoint": "minio:9000",
-        },
-        store_method="s3",
-
+        }
     )
-    assert task._structured
-    assert task._images
+    assert task._embedding
     assert task._store_method == "s3"
     assert task._extra_params["endpoint"] == "minio:9000"
     assert task._extra_params["access_key"] == "foo"
@@ -32,18 +29,16 @@ def test_store_task_initialization():
 
 
 def test_store_task_str_representation():
-    task = StoreTask(
-        structured=True, 
-        images=True, 
-        store_method="minio", 
+    task = StoreEmbedTask(
+        embedding=True, 
+        store_method="minio",
         extra_params={
-            "endpoint":"minio:9000"
+            "endpoint": "minio:9000"
         }
-        )
+    )
     expected_str = (
-        "Store Task:\n"
-        "  store structured types: True\n"
-        "  store image types: True\n"
+        "Store Embed Task:\n"
+        "  store embedding: True\n"
         "  store method: minio\n"
         "  endpoint: minio:9000\n"
     )
@@ -54,27 +49,25 @@ def test_store_task_str_representation():
 
 
 @pytest.mark.parametrize(
-    "structured, images, store_method, extra_param_1, extra_param_2",
+    "embedding, store_method, extra_param_1, extra_param_2",
     [
-        (True, True, "minio", "foo", "bar"),
-        (False, True, "minio", "foo", "bar"),
-        (False, False, "minio", "foo", "bar"),
-        (False, True, "s3", "foo", "bar"),
-        (None, True, "s3", "foo", "bar"),
-        (True, None, "s3", "foo", "bar"),
-        (None, None, "minio", "foo", "bar"),
+        (True, "minio", "foo", "bar"),
+        (True, "minio", "foo", "bar"),
+        (False, "minio", "foo", "bar"),
+        (True, "s3", "foo", "bar"),
+        (True, "s3", "foo", "bar"),
+        (None, "s3", "foo", "bar"),
+        (None, "minio", "foo", "bar"),
     ],
 )
 def test_store_task_to_dict(
-    structured,
-    images,
+    embedding,
     store_method,
     extra_param_1,
     extra_param_2,
 ):
-    task = StoreTask(
-        structured=structured,
-        images=images,
+    task = StoreEmbedTask(
+        embedding=embedding,
         store_method=store_method,
         extra_params={
             "extra_param_1": extra_param_1,
@@ -82,10 +75,9 @@ def test_store_task_to_dict(
         }
     )
 
-    expected_dict = {"type": "store", "task_properties": {"extra_params": {}}}
+    expected_dict = {"type": "store_embedding", "task_properties": {"extra_params": {}}}
 
-    expected_dict["task_properties"]["structured"] = structured
-    expected_dict["task_properties"]["images"] = images
+    expected_dict["task_properties"]["embedding"] = embedding
     expected_dict["task_properties"]["method"] = store_method or "minio"
     expected_dict["task_properties"]["extra_params"]["extra_param_1"] = extra_param_1
     expected_dict["task_properties"]["extra_params"]["extra_param_2"] = extra_param_2
