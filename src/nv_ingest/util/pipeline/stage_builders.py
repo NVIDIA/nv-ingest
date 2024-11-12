233b877e3a15ab56f7762e5e4656539f743e3b17
Devin Robison <drobison@nvidia.com>
2024-11-12 15:17:21 -0700

    PR feedback updates

--- src/nv_ingest/util/pipeline/stage_builders.py
+++ src/nv_ingest/util/pipeline/stage_builders.py
@@ -351,8 +351,7 @@ def add_image_caption_stage(pipe, morpheus_pipeline_config, ingest_config, defau
         "",
     )
 
-    endpoint_url = os.environ.get("NVIDIA_BUILD_CAPTION_ENDPOINT",
-                                  "https://ai.api.nvidia.com/v1/gr/meta/llama-3.2-90b-vision-instruct/chat/completions")
+    endpoint_url = os.environ.get("VLM_CAPTIONING_ENDPOINT")
 
     image_caption_config = ingest_config.get(
         "image_caption_extraction_module",
