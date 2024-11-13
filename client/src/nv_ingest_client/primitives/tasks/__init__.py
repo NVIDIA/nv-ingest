a53222d1b2e982b5a214bd6cedc8a7ff2c3930e5
edknv <edwardk@nvidia.com>
2024-11-13 13:28:15 -0800

    Extract tables and charts by default in ingestor extract

--- client/src/nv_ingest_client/primitives/tasks/__init__.py
+++ client/src/nv_ingest_client/primitives/tasks/__init__.py
@@ -3,12 +3,14 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .caption import CaptionTask
+from .chart_extraction import ChartExtractionTask
 from .dedup import DedupTask
 from .embed import EmbedTask
 from .extract import ExtractTask
 from .filter import FilterTask
 from .split import SplitTask
 from .store import StoreTask
+from .table_extraction import TableExtractionTask
 from .task_base import Task
 from .task_base import TaskType
 from .task_base import is_valid_task_type
@@ -17,10 +19,12 @@ from .vdb_upload import VdbUploadTask
 
 __all__ = [
     "CaptionTask",
+    "ChartExtractionTask",
     "ExtractTask",
     "is_valid_task_type",
     "SplitTask",
     "StoreTask",
+    "TableExtractionTask",
     "Task",
     "task_factory",
     "TaskType",
