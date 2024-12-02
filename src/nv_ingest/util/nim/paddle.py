2e9a8ce3a386e1c2f65e4d5d269ed5495198db0f
edknv <edwardk@nvidia.com>
2024-12-02 14:21:56 -0800

    grpc - use new ga api for newer versions of paddle

--- src/nv_ingest/util/nim/paddle.py
+++ src/nv_ingest/util/nim/paddle.py
@@ -1,9 +1,15 @@
-from typing import Dict, Any, Optional
-import numpy as np
+import json
 import logging
+from typing import Any
+from typing import Dict
+from typing import Optional
+
+import numpy as np
+import packaging
 
 from nv_ingest.util.image_processing.transforms import base64_to_numpy
-from nv_ingest.util.nim.helpers import ModelInterface, preprocess_image_for_paddle
+from nv_ingest.util.nim.helpers import ModelInterface
+from nv_ingest.util.nim.helpers import preprocess_image_for_paddle
 
 logger = logging.getLogger(__name__)
 
@@ -124,22 +130,10 @@ class PaddleOCRModelInterface(ModelInterface):
 
         if (protocol == 'grpc'):
             logger.debug("Parsing output from gRPC PaddleOCR model")
-            # Convert bytes output to string
-            if isinstance(response, np.ndarray):
-                if response.dtype.type is np.object_ or response.dtype.type is np.bytes_:
-                    output_strings = [
-                        output[0].decode("utf-8") if isinstance(output, bytes) else str(output) for output in response
-                    ]
-                    return " ".join(output_strings)
-                else:
-                    output_bytes = response.tobytes()
-                    output_str = output_bytes.decode("utf-8")
-                    return output_str
-            else:
-                raise ValueError("Unexpected response format: response is not a NumPy array.")
+            return self._extract_content_from_paddle_grpc_response(response)
         elif (protocol == 'http'):
             logger.debug("Parsing output from HTTP PaddleOCR model")
-            return self._extract_content_from_paddle_response(response)
+            return self._extract_content_from_paddle_http_response(response)
         else:
             raise ValueError("Invalid protocol specified. Must be 'grpc' or 'http'.")
 
@@ -224,5 +218,29 @@ class PaddleOCRModelInterface(ModelInterface):
             text_detections = json_response["data"][0]["text_detections"]
             # "simple" format
             content = " ".join(t["text_prediction"]["text"] for t in text_detections)
+
+        return content
+
+    def _extract_content_from_paddle_grpc_response(self, response):
+        # Convert bytes output to string
+        if not isinstance(response, np.ndarray):
+            raise ValueError("Unexpected response format: response is not a NumPy array.")
+
+        if self._is_version_early_access_legacy_api():
+            if response.dtype.type is np.object_ or response.dtype.type is np.bytes_:
+                output_strings = [
+                    output[0].decode("utf-8") if isinstance(output, bytes) else str(output) for output in response
+                ]
+                content = " ".join(output_strings)
+            else:
+                output_bytes = response.tobytes()
+                output_str = output_bytes.decode("utf-8")
+                content = output_str
+        else:
+            bboxes_bytestr, texts_bytestr, _ = response
+            bboxes_list = json.loads(bboxes_bytestr.decode("utf8"))[0]
+            texts_list = json.loads(texts_bytestr.decode("utf8"))[0]
+            # "simple" format
+            content = " ".join(texts_list)
 
         return content
