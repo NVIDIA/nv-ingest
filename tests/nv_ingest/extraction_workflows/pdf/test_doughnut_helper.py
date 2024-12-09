792b7245402736dc710a44572664370cbda84e8d
edknv <edwardk@nvidia.com>
2024-12-09 14:12:41 -0800

    update unit tests

--- tests/nv_ingest/extraction_workflows/pdf/test_doughnut_helper.py
+++ tests/nv_ingest/extraction_workflows/pdf/test_doughnut_helper.py
@@ -12,7 +12,6 @@ from nv_ingest.extraction_workflows.pdf.doughnut_helper import preprocess_and_se
 from nv_ingest.schemas.metadata_schema import AccessLevelEnum
 from nv_ingest.schemas.metadata_schema import TextTypeEnum
 from nv_ingest.util.nim import doughnut as doughnut_utils
-from nv_ingest.util.nim.helpers import call_image_inference_model
 from nv_ingest.util.pdf.metadata_aggregators import Base64Image
 from nv_ingest.util.pdf.metadata_aggregators import LatexTable
 
@@ -37,10 +36,10 @@ def sample_pdf_stream():
 
 
 @patch(f"{_MODULE_UNDER_TEST}.create_inference_client")
-@patch(f"{_MODULE_UNDER_TEST}.call_image_inference_model")
-def test_doughnut_text_extraction(mock_call_inference, mock_create_client, sample_pdf_stream, document_df):
-    mock_create_client.return_value = MagicMock()
-    mock_call_inference.return_value = "<x_0><y_1>testing<x_10><y_20><class_Text>"
+def test_doughnut_text_extraction(mock_client, sample_pdf_stream, document_df):
+    mock_client_instance = MagicMock()
+    mock_client.return_value = mock_client_instance
+    mock_client_instance.infer.return_value = "<x_0><y_1>testing<x_10><y_20><class_Text>"
 
     result = doughnut(
         pdf_stream=sample_pdf_stream,
@@ -52,8 +51,6 @@ def test_doughnut_text_extraction(mock_call_inference, mock_create_client, sampl
         doughnut_config=MagicMock(doughnut_batch_size=1),
     )
 
-    mock_call_inference.assert_called()
-
     assert len(result) == 1
     assert result[0][0].value == "text"
     assert result[0][1]["content"] == "testing"
@@ -61,10 +58,10 @@ def test_doughnut_text_extraction(mock_call_inference, mock_create_client, sampl
 
 
 @patch(f"{_MODULE_UNDER_TEST}.create_inference_client")
-@patch(f"{_MODULE_UNDER_TEST}.call_image_inference_model")
-def test_doughnut_table_extraction(mock_call_inference, mock_create_client, sample_pdf_stream, document_df):
-    mock_create_client.return_value = MagicMock()
-    mock_call_inference.return_value = "<x_17><y_0>table text<x_1007><y_1280><class_Table>"
+def test_doughnut_table_extraction(mock_client, sample_pdf_stream, document_df):
+    mock_client_instance = MagicMock()
+    mock_client.return_value = mock_client_instance
+    mock_client_instance.infer.return_value = "<x_17><y_0>table text<x_1007><y_1280><class_Table>"
 
     result = doughnut(
         pdf_stream=sample_pdf_stream,
@@ -76,8 +73,6 @@ def test_doughnut_table_extraction(mock_call_inference, mock_create_client, samp
         doughnut_config=MagicMock(doughnut_batch_size=1),
     )
 
-    mock_call_inference.assert_called()
-
     assert len(result) == 2
     assert result[0][0].value == "structured"
     assert result[0][1]["content"] == "table text"
@@ -88,10 +83,10 @@ def test_doughnut_table_extraction(mock_call_inference, mock_create_client, samp
 
 
 @patch(f"{_MODULE_UNDER_TEST}.create_inference_client")
-@patch(f"{_MODULE_UNDER_TEST}.call_image_inference_model")
-def test_doughnut_image_extraction(mock_call_inference, mock_create_client, sample_pdf_stream, document_df):
-    mock_create_client.return_value = MagicMock()
-    mock_call_inference.return_value = "<x_17><y_0><x_1007><y_1280><class_Picture>"
+def test_doughnut_image_extraction(mock_client, sample_pdf_stream, document_df):
+    mock_client_instance = MagicMock()
+    mock_client.return_value = mock_client_instance
+    mock_client_instance.infer.return_value = "<x_17><y_0><x_1007><y_1280><class_Picture>"
 
     result = doughnut(
         pdf_stream=sample_pdf_stream,
@@ -103,8 +98,6 @@ def test_doughnut_image_extraction(mock_call_inference, mock_create_client, samp
         doughnut_config=MagicMock(doughnut_batch_size=1),
     )
 
-    mock_call_inference.assert_called()
-
     assert len(result) == 2
     assert result[0][0].value == "image"
     assert result[0][1]["content"][:10] == "iVBORw0KGg"  # PNG format header
@@ -115,9 +108,7 @@ def test_doughnut_image_extraction(mock_call_inference, mock_create_client, samp
 
 
 @patch(f"{_MODULE_UNDER_TEST}.pdfium_pages_to_numpy")
-@patch(f"{_MODULE_UNDER_TEST}.call_image_inference_model")
-def test_preprocess_and_send_requests(mock_call_inference, mock_pdfium_pages_to_numpy):
-    mock_call_inference.return_value = ["<x_0><y_1>testing<x_10><y_20><class_Text>"] * 3
+def test_preprocess_and_send_requests(mock_pdfium_pages_to_numpy):
     mock_pdfium_pages_to_numpy.return_value = (np.array([[1], [2], [3]]), [0, 1, 2])
 
     mock_client = MagicMock()
@@ -131,14 +122,12 @@ def test_preprocess_and_send_requests(mock_call_inference, mock_pdfium_pages_to_
         isinstance(item, tuple) and len(item) == 3 for item in result
     ), "Each entry should be a tuple with 3 items"
 
-    mock_call_inference.assert_called()
-
 
 @patch(f"{_MODULE_UNDER_TEST}.create_inference_client")
-@patch(f"{_MODULE_UNDER_TEST}.call_image_inference_model")
-def test_doughnut_text_extraction_bboxes(mock_call_inference, mock_create_client, sample_pdf_stream, document_df):
-    mock_create_client.return_value = MagicMock()
-    mock_call_inference.return_value = (
+def test_doughnut_text_extraction_bboxes(mock_client, sample_pdf_stream, document_df):
+    mock_client_instance = MagicMock()
+    mock_client.return_value = mock_client_instance
+    mock_client_instance.infer.return_value = (
         "<x_0><y_1>testing0<x_10><y_20><class_Title><x_30><y_40>testing1<x_50><y_60><class_Text>"
     )
 
@@ -151,8 +140,6 @@ def test_doughnut_text_extraction_bboxes(mock_call_inference, mock_create_client
         text_depth="page",
         doughnut_config=MagicMock(doughnut_batch_size=1),
     )
-
-    mock_call_inference.assert_called()
 
     assert len(result) == 1
     assert result[0][0].value == "text"
