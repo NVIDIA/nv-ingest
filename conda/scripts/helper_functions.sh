2035caf8c037d33dc96645da40e18d5c9895b33b
Devin Robison <drobison@nvidia.com>
2024-12-13 14:51:49 -0700

    Add copyright headers

--- conda/scripts/helper_functions.sh
+++ conda/scripts/helper_functions.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 # Fail on errors and undefined variables
 set -euo pipefail
