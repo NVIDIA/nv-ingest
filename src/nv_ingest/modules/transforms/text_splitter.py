5dd128cb4a3ffccff1198b099a16258e9d5d230a
Chris Jarrett <cjarrett@dgx-a100-01.aselab.nvidia.com>
2025-03-07 08:02:39 -0800

    Remove absolute paths

--- src/nv_ingest/modules/transforms/text_splitter.py
+++ src/nv_ingest/modules/transforms/text_splitter.py
@@ -125,11 +125,11 @@ def _text_splitter(builder: mrc.Builder):
             if os.path.exists(os.path.join(model_predownload_path, "llama-3.2-1b/tokenizer/tokenizer.json")) and (
                 tokenizer is None or tokenizer == "meta-llama/Llama-3.2-1B"
             ):
-                tokenizer = "/workspace/models/llama-3.2-1b/tokenizer/"
+                tokenizer = os.path.join(model_predownload_path, "llama-3.2-1b/tokenizer/tokenizer.json")
             elif os.path.exists(
                 os.path.join(model_predownload_path, "e5-large-unsupervised/tokenizer/tokenizer.json")
             ) and (tokenizer is None or tokenizer == "intfloat/e5-large-unsupervised"):
-                tokenizer = "/workspace/models/e5-large-unsupervised/tokenizer/"
+                tokenizer = os.path.join(model_predownload_path, "e5-large-unsupervised/tokenizer/tokenizer.json")
 
             tokenizer_model = AutoTokenizer.from_pretrained(tokenizer, token=hf_access_token)
 
