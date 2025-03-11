57b4328d39bfa8928fc2c0d723c3216048d4be3f
Devin Robison <drobison@nvidia.com>
2025-03-11 10:46:49 -0600

    More unit tests

--- api/src/nv_ingest_api/internal/extract/pdf/engines/pdf_helpers/__init__.py
+++ api/src/nv_ingest_api/internal/extract/pdf/engines/pdf_helpers/__init__.py
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES.
 # All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
 import base64
