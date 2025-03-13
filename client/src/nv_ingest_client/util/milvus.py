b898fec62a13a78e10820dce68cc4d0409af3bbd
Julio Perez <37191411+jperez999@users.noreply.github.com>
2025-03-12 20:06:04 -0400

    this adds 1% to ranker recall (#555)

--- client/src/nv_ingest_client/util/milvus.py
+++ client/src/nv_ingest_client/util/milvus.py
@@ -338,7 +338,7 @@ def create_nvingest_index_params(
                 metric_type="L2",
                 params={
                     "intermediate_graph_degree": 128,
-                    "graph_degree": 64,
+                    "graph_degree": 100,
                     "build_algo": "NN_DESCENT",
                     "adapt_for_cpu": "false" if gpu_search else "true",
                 },
@@ -1062,6 +1062,7 @@ def nvingest_retrieval(
     nv_ranker_truncate: str = "END",
     nv_ranker_top_k: int = 5,
     nv_ranker_max_batch_size: int = 64,
+    nv_ranker_candidate_multiple: int = 10,
 ):
     """
     This function takes the input queries and conducts a hybrid/dense
@@ -1122,7 +1123,7 @@ def nvingest_retrieval(
     client = MilvusClient(milvus_uri)
     nv_ranker_top_k = top_k
     if nv_ranker:
-        top_k = top_k * 2
+        top_k = top_k * nv_ranker_candidate_multiple
     if milvus_uri.endswith(".db"):
         local_index = True
     if hybrid:
