ab7193ddd900806826ec5915648a8ed6960ac690
Devin Robison <drobison@nvidia.com>
2025-03-14 16:00:44 -0600

    Checkpoint

--- src/nv_ingest/schemas/embed_extractions_schema.py
+++ src/nv_ingest/schemas/embed_extractions_schema.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 class EmbedExtractionsSchema(BaseModel):
     api_key: str = "api_key"
-    batch_size: int = 8192
+    batch_size: int = 8
     embedding_model: str = "nvidia/nv-embedqa-e5-v5"
     embedding_nim_endpoint: str = "http://embedding:8000/v1"
     encoding_format: str = "float"
