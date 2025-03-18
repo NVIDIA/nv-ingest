a6cbe777da39628d39a7dd816c06aef78ad65941
edknv <edwardk@nvidia.com>
2025-03-17 17:33:03 -0700

    build conda from source

--- tests/integration/test_library_mode_examle.py
+++ tests/integration/test_library_mode_examle.py
@@ -4,4 +4,11 @@ import subprocess
 def test_library_mode_extract_pdf():
     result = subprocess.run(["python", "examples/library_mode_example.py"], capture_output=True, text=True)
 
-    assert "Chasing a squirrel" in result.stdout
+    try:
+        assert "Chasing a squirrel" in result.stdout
+    except:
+        print("Standard Output:")
+        print(result.stdout)
+        print("Standard Error:")
+        print(result.stderr)
+        raise
