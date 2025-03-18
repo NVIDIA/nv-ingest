3289f05562339b1453f6136f0cc4778665c35fe0
edknv <edwardk@nvidia.com>
2025-03-18 09:31:48 -0700

    remove dev channel and pip install unstructured

--- tests/integration/test_library_mode_examle.py
+++ tests/integration/test_library_mode_examle.py
@@ -4,11 +4,4 @@ import subprocess
 def test_library_mode_extract_pdf():
     result = subprocess.run(["python", "examples/library_mode_example.py"], capture_output=True, text=True)
 
-    try:
-        assert "Chasing a squirrel" in result.stdout
-    except:
-        print("Standard Output:")
-        print(result.stdout)
-        print("Standard Error:")
-        print(result.stderr)
-        raise
+    assert "Chasing a squirrel" in result.stdout
