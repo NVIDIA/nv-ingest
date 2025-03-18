c54decb0c8bfea7c325425663762306c1e03f1da
edknv <edwardk@nvidia.com>
2025-03-18 10:08:40 -0700

    display subprocess stdout/stderr

--- tests/integration/test_library_mode_examle.py
+++ tests/integration/test_library_mode_examle.py
@@ -4,5 +4,10 @@ import subprocess
 def test_library_mode_extract_pdf():
     process = subprocess.run(["python", "examples/library_mode_example.py"], capture_output=True, text=True)
 
-    assert process.returncode == 0
-    assert "Chasing a squirrel" in process.stdout
+    try:
+        assert process.returncode == 0
+        assert "Chasing a squirrel" in process.stdout
+    except:
+        print(process.stdout)
+        print(process.stderr)
+        raise
