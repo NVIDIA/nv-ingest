fb793a58643c9df36f979eb231262ff5a0f2ff60
edknv <edwardk@nvidia.com>
2025-03-22 12:28:28 -0700

    run only ingestor test

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -1,17 +1,17 @@
 import subprocess
 
 
-def test_launch_libmode_service():
-    process = subprocess.run(["python", "examples/launch_libmode_service.py"], capture_output=True, text=True)
-
-    try:
-        assert process.returncode == 0
-        # pdfium text
-        assert "A sample document with headings and placeholder text" in process.stdout
-    except:
-        print(process.stdout)
-        print(process.stderr)
-        raise
+#def test_launch_libmode_service():
+#    process = subprocess.run(["python", "examples/launch_libmode_service.py"], capture_output=True, text=True)
+#
+#    try:
+#        assert process.returncode == 0
+#        # pdfium text
+#        assert "A sample document with headings and placeholder text" in process.stdout
+#    except:
+#        print(process.stdout)
+#        print(process.stderr)
+#        raise
 
 
 def test_launch_libmode_and_run_ingestor():
