1f8e5fe130a16802ee08bc36110939e0e924b9cc
edknv <edwardk@nvidia.com>
2025-03-24 21:35:34 -0700

    add nemoretriever parse test

--- tests/integration/test_extract_pdf.py
+++ tests/integration/test_extract_pdf.py
@@ -9,7 +9,7 @@ from nv_ingest.framework.orchestration.morpheus.util.pipeline.pipeline_runners i
 from nv_ingest.framework.orchestration.morpheus.util.pipeline.pipeline_runners import start_pipeline_subprocess
 
 
-def test_extract_pdf():
+def test_pdfium_extract_pdf():
     config = PipelineCreationSchema()
     pipeline_process = start_pipeline_subprocess(config, stderr=sys.stderr, stdout=sys.stdout)
     time.sleep(10)
@@ -38,3 +38,37 @@ def test_extract_pdf():
 
     results = ingestor.ingest()
     assert len(results) == 1
+    assert len(results[0]) == 3
+
+
+def test_nemoretriever_parse_extract_pdf():
+    config = PipelineCreationSchema()
+    pipeline_process = start_pipeline_subprocess(config, stderr=sys.stderr, stdout=sys.stdout)
+    time.sleep(10)
+
+    assert pipeline_process.poll() is None, "Error running pipeline subprocess."
+
+    client = NvIngestClient(
+        message_client_allocator=SimpleClient,
+        message_client_port=7671,
+        message_client_hostname="localhost",
+    )
+
+    ingestor = (
+        Ingestor(client=client)
+        .files("./data/multimodal_test.pdf")
+        .extract(
+            extract_method="nemoretriever_parse",
+            extract_text=True,
+            extract_tables=True,
+            extract_charts=True,
+            extract_images=True,
+            paddle_output_format="markdown",
+            extract_infographics=True,
+            text_depth="page",
+        )
+    )
+
+    results = ingestor.ingest()
+    assert len(results) == 1
+    assert len(results[0]) == 3
