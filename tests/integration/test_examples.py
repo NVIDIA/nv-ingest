325ee64ed9ee7d65d85a55b939893601f8c79d0c
edknv <edwardk@nvidia.com>
2025-03-24 20:28:11 -0700

    add integration test running example script

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -1,2 +1,14 @@
-def test_placeholder():
-    assert True
+import subprocess
+
+
+def test_launch_libmode_and_run_ingestor():
+    process = subprocess.run(["python", "./examples/launch_libmode_and_run_ingestor.py"], capture_output=True, text=True)
+
+    try:
+        assert process.returncode == 0
+        # pdfium text
+        assert "A sample document with headings and placeholder text" in process.stdout
+    except:
+        print(process.stdout)
+        print(process.stderr)
+        raise
