050de1b8e41571a5ce46db7901f0ec7e2433b01f
edknv <edwardk@nvidia.com>
2025-04-01 02:05:36 -0700

    add unstaged file

---
+++ tests/integration/conftest.py
@@ -0,0 +1,27 @@
+import sys
+import time
+
+import pytest
+
+from nv_ingest.framework.orchestration.morpheus.util.pipeline.pipeline_runners import PipelineCreationSchema
+from nv_ingest.framework.orchestration.morpheus.util.pipeline.pipeline_runners import start_pipeline_subprocess
+
+
+@pytest.fixture
+def pipeline_process():
+    config = PipelineCreationSchema()
+    process = start_pipeline_subprocess(
+        config,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+
+    time.sleep(5)
+
+    if process.poll() is not None:
+        raise RuntimeError("Error running pipeline subprocess.")
+
+    yield process
+
+    process.kill()
+    process.wait()
