4354b8e655c52da0b2a04f6eb95a000aecd72f57
Jeremy Dyer <jdye64@gmail.com>
2025-04-17 11:41:26 -0400

    Add a go template that makes it much more clean to autogenerate the docs and also keep the static content where expected

--- helm/update_helm_readme.sh
+++ helm/update_helm_readme.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-# Create a temporary file for the helm-docs output
-helm-docs --output-file=README.md.suffix
+# script for updating the helm README.md file with the helm-docs
+# More complicated tasks are envisioned and hence the existence of this script given its simple nature
 
-# Output the <!-- BEGIN: Auto-generated helm-docs --> and <!-- END: Auto-generated helm-docs --> back into the file
-# Prepend the BEGIN comment
-{ echo '<!-- BEGIN: Auto-generated helm-docs -->'; cat README.md.suffix; } > temp && mv temp README.md.suffix
-
-# Append the END comment
-echo '<!-- END: Auto-generated helm-docs -->' >> README.md.suffix
-
-# Split the original README.md at "<!-- BEGIN: Auto-generated helm-docs -->"
-awk '/<!-- BEGIN: Auto-generated helm-docs -->/{exit} {print}' README.md > README.md.prefix
-
-# Combine the files
-cat README.md.prefix README.md.suffix > README.md
-
-# Clean up temporary files
-rm README.md.prefix README.md.suffix
+helm-docs
