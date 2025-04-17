0349c9dbdfd8fbdd13264213d23b7bc73ad929e0
Jeremy Dyer <jdye64@gmail.com>
2025-04-17 11:30:40 -0400

    Update script for autogenerating parameter docs

--- helm/update_helm_readme.sh
+++ helm/update_helm_readme.sh
@@ -3,8 +3,15 @@
 # Create a temporary file for the helm-docs output
 helm-docs --output-file=README.md.suffix
 
-# Split the original README.md at "### Deployment parameters"
-awk '/### Deployment parameters/{exit} {print}' README.md > README.md.prefix
+# Output the <!-- BEGIN: Auto-generated helm-docs --> and <!-- END: Auto-generated helm-docs --> back into the file
+# Prepend the BEGIN comment
+{ echo '<!-- BEGIN: Auto-generated helm-docs -->'; cat README.md.suffix; } > temp && mv temp README.md.suffix
+
+# Append the END comment
+echo '<!-- END: Auto-generated helm-docs -->' >> README.md.suffix
+
+# Split the original README.md at "<!-- BEGIN: Auto-generated helm-docs -->"
+awk '/<!-- BEGIN: Auto-generated helm-docs -->/{exit} {print}' README.md > README.md.prefix
 
 # Combine the files
 cat README.md.prefix README.md.suffix > README.md
