c2b3166203c2435fe82341d1412e8338010749e4
Jeremy Dyer <jdye64@gmail.com>
2025-04-22 11:23:42 -0400

    Adjust nv-ingest health endpoint to allow for configurable readiness … (#641)

--- src/nv_ingest/api/v1/health.py
+++ src/nv_ingest/api/v1/health.py
@@ -11,9 +11,18 @@ from fastapi.responses import JSONResponse
 
 from nv_ingest_api.internal.primitives.nim.model_interface.helpers import is_ready
 
-logger = logging.getLogger("uvicorn")
+# logger = logging.getLogger("uvicorn")
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
+
+# List of ALL of the HTTP environment variable endpoints that should be checked
+READY_CHECK_ENV_VAR_MAP = {
+    "paddle": "PADDLE_HTTP_ENDPOINT",
+    "yolox_graphic_elements": "YOLOX_GRAPHIC_ELEMENTS_HTTP_ENDPOINT",
+    "yolox_page_elements": "YOLOX_HTTP_ENDPOINT",
+    "yolox_table_structure": "YOLOX_TABLE_STRUCTURE_HTTP_ENDPOINT",
+}
 
 
 @router.get(
@@ -43,11 +52,7 @@ async def get_ready_state() -> dict:
     # "Ready" to use means this.
     # 1. nv-ingest FastAPI is live, check you are here nothing to do.
     # 2. Morpheus pipeline is up and running
-    # 3. Yolox NIM "ready" health endpoint returns 200 status code
-    # 4. Deplot NIM "ready" health endpoint returns 200 status code
-    # 5. Cached NIM "ready" health endpoint returns 200 status code
-    # 6. PaddleOCR NIM "ready" health endpoint returns 200 status code
-    # 7. Embedding NIM "ready" health endpoint returns 200 status code
+    # 3. NIMs that are configured by the service are reporting "ready"
     # After all of those are "ready" this service returns "ready" as well
     # Otherwise a HTTP 503 Service not Available response is returned.
 
@@ -61,32 +66,49 @@ async def get_ready_state() -> dict:
     # the pipeline.
     morpheus_pipeline_ready = True
 
-    # We give the users an option to disable checking all distributed services for "readiness"
-    check_all_components = os.getenv("READY_CHECK_ALL_COMPONENTS", "True").lower()
-    if check_all_components in ["1", "true", "yes"]:
-        yolox_page_elements_ready = is_ready(os.getenv("YOLOX_HTTP_ENDPOINT", None), "/v1/health/ready")
-        yolox_graphic_elements_ready = is_ready(
-            os.getenv("YOLOX_GRAPHIC_ELEMENTS_HTTP_ENDPOINT", None), "/v1/health/ready"
-        )
-        paddle_ready = is_ready(os.getenv("PADDLE_HTTP_ENDPOINT", None), "/v1/health/ready")
+    # Components that the service should check for "ready"
+    # Possible options are
+    # 1. empty/none -> This equates to disabling ready checks
+    # 2. all/ALL -> This equates to checking the environment variables and checking all configured services for "ready"
+    # 3. {comma_delimited_list} -> Comma delimited list of {NIM_HTTP_ENDPOINTS} that should be checked for ready
+    components_to_check = os.getenv("COMPONENTS_TO_READY_CHECK", "ALL").upper()
 
-        if (
-            ingest_ready
-            and morpheus_pipeline_ready
-            and yolox_page_elements_ready
-            and yolox_graphic_elements_ready
-            and paddle_ready
-        ):
+    if components_to_check == "" or components_to_check is None:
+        # Ready checks disabled. Immdiately return "ready" status
+        return JSONResponse(content={"ready": True}, status_code=200)
+    else:
+        # Determine the list of components to check, either ALL or a specified list
+        endpoint_nim_name_map = {}
+        if components_to_check == "ALL":
+            # Gather all the known HTTP env endpoints
+            for nim_name, nim_env_var in READY_CHECK_ENV_VAR_MAP.items():
+                endpoint_url = os.getenv(nim_env_var, None)
+                endpoint_nim_name_map[endpoint_url] = nim_name
+        else:
+            # This will be a list of env variables for the http endpoints to check
+            for env_var in components_to_check.split(","):
+                env_var = env_var.strip()
+                endpoint_url = os.getenv(env_var, None)
+
+                # Get the user friendly name for the NIM from the endpoints map
+                nim_name = next((k for k, v in READY_CHECK_ENV_VAR_MAP.items() if v == env_var), None)
+                endpoint_nim_name_map[endpoint_url] = nim_name
+
+        # Check the endpoints for their readiness
+        ready_statuses = {"ingest_ready": ingest_ready, "morpheus_pipeline_ready": morpheus_pipeline_ready}
+        ready_to_work = True  # consider nv-ingest ready until an endpoint proves otherwise
+        for endpoint, nim_name in endpoint_nim_name_map.items():
+            endpoint_ready = is_ready(endpoint, "/v1/health/ready")
+            if not endpoint_ready:
+                logger.debug(f"Not ready for work. NIM endpoint: '{endpoint}' reporting not ready.")
+                ready_to_work = False
+                ready_statuses[nim_name + "_ready"] = False
+            else:
+                ready_statuses[nim_name + "_ready"] = True
+
+        # Build the response for the client
+        if ready_to_work:
             return JSONResponse(content={"ready": True}, status_code=200)
         else:
-            ready_statuses = {
-                "ingest_ready": ingest_ready,
-                "morpheus_pipeline_ready": morpheus_pipeline_ready,
-                "page_elemenst_ready": yolox_page_elements_ready,
-                "graphic_elements_ready": yolox_graphic_elements_ready,
-                "paddle_ready": paddle_ready,
-            }
             logger.debug(f"Ready Statuses: {ready_statuses}")
             return JSONResponse(content=ready_statuses, status_code=503)
-    else:
-        return JSONResponse(content={"ready": True}, status_code=200)
