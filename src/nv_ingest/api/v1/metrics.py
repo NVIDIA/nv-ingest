19aa803c558465491e7b2cdae9bedbc48285a97d
Jeremy Dyer <jdye64@gmail.com>
2025-05-08 09:19:49 -0400

    checkpoint commit

--- src/nv_ingest/api/v1/metrics.py
+++ src/nv_ingest/api/v1/metrics.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 import logging
 
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, status
 from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 
 router = APIRouter()
@@ -16,6 +16,14 @@ REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests", ["method",
 REQUEST_LATENCY = Histogram("http_request_duration_seconds", "Request latency", ["method", "endpoint"])
 
 
-@router.get("/metrics")
+@router.get(
+    "/metrics",
+    tags=["Health"],
+    summary="Provide prometheus formatted metrics for consumption",
+    description="""
+        Provide prometheus formatted metrics for consumption by a prometheus scraping server.
+    """,
+    status_code=status.HTTP_200_OK,
+)
 def metrics():
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
