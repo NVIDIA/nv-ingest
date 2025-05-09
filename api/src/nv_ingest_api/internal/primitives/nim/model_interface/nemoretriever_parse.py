1c946d341d16ccfd96b3368b57b2d0761f29bac6
Vibhu Jawa <vjawa@nvidia.com>
2025-05-09 06:54:07 -0700

    also chaneg image_url
    Signed-off-by: Vibhu Jawa <vjawa@nvidia.com>

--- api/src/nv_ingest_api/internal/primitives/nim/model_interface/nemoretriever_parse.py
+++ api/src/nv_ingest_api/internal/primitives/nim/model_interface/nemoretriever_parse.py
@@ -198,7 +198,7 @@ class NemoRetrieverParseModelInterface(ModelInterface):
                         {
                             "type": "image_url",
                             "image_url": {
-                                "url": f"data:image/png;base64,{b64_img}",
+                                "url": f"data:image/jpeg;base64,{b64_img}",
                             },
                         }
                     ],
