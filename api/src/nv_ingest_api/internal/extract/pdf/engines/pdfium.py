93dc5742da0f3f9c515bbc5fdd5c1e4124d4bff1
Edward Kim <edwardk@nvidia.com>
2025-06-19 03:24:36 +0000

    max dimensions

--- api/src/nv_ingest_api/internal/extract/pdf/engines/pdfium.py
+++ api/src/nv_ingest_api/internal/extract/pdf/engines/pdfium.py
@@ -531,7 +531,11 @@ def pdfium_extractor(
             # Full page image extraction
             if extract_page_as_image:
                 page_text = _extract_page_text(page)
-                image, _ = pdfium_pages_to_numpy([page], trace_info=execution_trace_log)
+                if (page.height > 16384) or (page.width > 16384):
+                    scale_tuple = (16384, 16384)
+                else:
+                    scale_tuple = None
+                image, _ = pdfium_pages_to_numpy([page], scale_tuple=scale_tuple, trace_info=execution_trace_log)
                 base64_image = numpy_to_base64(image[0])
                 if len(base64_image) > 2**24 - 1:
                     base64_image, _ = scale_image_to_encoding_size(base64_image, max_base64_size=2**24 - 1)
