b1564232e47ee37dd7f49a4ab69e238d2fcb4043
Julio Perez <jperez@nvidia.com>
2025-09-10 20:14:52 +0000

    add flush streaming inserts

--- client/src/nv_ingest_client/util/vdb/milvus.py
+++ client/src/nv_ingest_client/util/vdb/milvus.py
@@ -903,6 +903,7 @@ def stream_insert_milvus(records, client: MilvusClient, collection_name: str, ba
     for idx in range(0, len(records), batch_size):
         client.insert(collection_name=collection_name, data=records[idx : idx + batch_size])
         count += len(records[idx : idx + batch_size])
+    client.flush(collection_name)
     logger.info(f"streamed {count} records")
 
 
@@ -923,6 +924,9 @@ def wait_for_index(collection_name: str, num_elements: int, client: MilvusClient
             for i in range(20):
                 new_indexed_rows = client.describe_index(collection_name, index_name)["indexed_rows"]
                 time.sleep(1)
+                logger.info(
+                    f"polling for indexed rows, {collection_name}, {index_name} -  {new_indexed_rows} / {num_elements}"
+                )
                 if new_indexed_rows == num_elements:
                     indexed_rows = new_indexed_rows
                     break
