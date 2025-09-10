e6fe37fb03edb67579214dbb1a4c64773cbc2fc1
ChrisJar <chris.jarrett.0@gmail.com>
2025-09-10 16:31:25 -0400

    Update api/src/nv_ingest_api/internal/schemas/meta/metadata_schema.py
    Co-authored-by: nkmcalli <nkmcalli@yahoo.com>

--- api/src/nv_ingest_api/internal/schemas/meta/metadata_schema.py
+++ api/src/nv_ingest_api/internal/schemas/meta/metadata_schema.py
@@ -184,7 +184,7 @@ class ImageMetadataSchema(BaseModelNoExt):
     """The bounding box of the image, in the format (x1,y1,x2,y2)."""
 
     image_location_max_dimensions: tuple = (0, 0)
-    """Max dimensions (x_max,y_max) of the bounding box of the image"""
+    """The maximum dimensions of the bounding box of the image, in the format (x_max,y_max)."""
 
     uploaded_image_url: str = ""
     """Mirrors source_metadata.source_location"""
