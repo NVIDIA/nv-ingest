d7154ed06699766b57a50bf12e338fb84f024856
Julio Perez <jperez@nvidia.com>
2025-09-15 20:13:11 +0000

    remove formatting change from milvus.py

--- client/src/nv_ingest_client/util/vdb/milvus.py
+++ client/src/nv_ingest_client/util/vdb/milvus.py
@@ -334,7 +334,6 @@ def create_nvingest_index_params(
                     "adapt_for_cpu": "false" if gpu_search else "true",
                 },
             )
-
         else:
             index_params.add_index(
                 field_name="vector",
