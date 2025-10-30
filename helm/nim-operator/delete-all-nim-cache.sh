a138c434bea6f918abd5081cd4febfec8c81e7e5
Jeremy Dyer <jdye64@nvidia.com>
2025-10-30 14:32:12 -0400

    Helm updates

--- helm/nim-operator/delete-all-nim-cache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Accept input parameters for namespaces
-NAMESPACE=${1:-nv-ingest}
-
-echo "Deleting all NimCache in namespace: $NAMESPACE"
-
-kubectl delete nimcache --all -n $NAMESPACE
--- helm/nim-operator/delete-all-nim-services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Accept input parameters for namespaces
-NAMESPACE=${1:-nv-ingest}
-
-echo "Deleting all NimServices in namespace: $NAMESPACE"
-
-kubectl delete nimservice --all -n $NAMESPACE
